fe9d85db2f42b5292f47c25b8e3ab23e32f74de3
Karina <sevynidd02@protonmail.com>
2024-05-18 19:14:49 +0200

    Liste

--- app/src/main/java/com/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/shoppinglist/MainActivity.kt
@@ -1,22 +1,41 @@
 package com.shoppinglist
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.shoppinglist.ui.theme.ShoppingListTheme
 
@@ -29,7 +48,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingListTheme {
                 Scaffold(modifier = Modifier.fillMaxSize(),
-                    topBar = { TopAppBar(title = { Text(text = "Test") }) },
+                    topBar = { TopAppBar(title = { Text(text = "Meine Listen") }) },
                     bottomBar = {
                         BottomAppBar {
                             NavigationBar {
@@ -44,13 +63,60 @@ class MainActivity : ComponentActivity() {
                                     })
                             }
                         }
+                    },
+                    floatingActionButton = {
+                        val context = LocalContext.current
+                        FloatingActionButton(
+                            onClick = {
+                                Toast.makeText(
+                                    context,
+                                    "Clicked on FAB",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        ) {
+                            Icon(Icons.Default.Add, "Add Lists")
+                        }
                     }
                 ) { innerPadding ->
-                    Text(
-                        text = "Hallöle",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(horizontal = 18.dp)
+                        ) {
+                            Content()
+                        }
+                    }
                 }
+            }
+        }
+    }
+
+    @Composable
+    fun Content() {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(12) { index ->
+                ListItem(
+                    modifier = Modifier.clip(shape = RoundedCornerShape(12.dp)),
+                    headlineContent = { Text("Liste ${index + 1}") },
+                    supportingContent = { Text("Notiz ${index + 1}") },
+                    trailingContent = { Text("Anzahl: ${(0..14).random()}") },
+                    leadingContent = {
+                        Icon(
+                            Icons.Default.ShoppingCart,
+                            contentDescription = "ShoppingCart",
+                        )
+                    },
+                    tonalElevation = 4.dp
+                )
             }
         }
     }
---
+++ app/src/main/java/com/shoppinglist/datastore/StoreData.kt
@@ -0,0 +1,28 @@
+package com.shoppinglist.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class StoreData(private val context: Context) {
+    companion object {
+        private val Context.datastore: DataStore<Preferences> by preferencesDataStore("DataStore")
+        val API_KEY = stringPreferencesKey("api_key")
+    }
+
+    val getApiKey: Flow<String?> = context.datastore.data
+        .map { preferences ->
+            preferences[API_KEY] ?: ""
+        }
+
+    suspend fun saveApiKey(key: String) {
+        context.datastore.edit { preferences ->
+            preferences[API_KEY] = key
+        }
+    }
+}
