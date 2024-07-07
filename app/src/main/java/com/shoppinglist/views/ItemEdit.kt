6b154e8ea8c6d72e4c387eb01479ef7b200f289d
Karina <sevynidd02@protonmail.com>
2024-07-07 14:27:59 +0200

    Added Focus

--- app/src/main/java/com/shoppinglist/views/ItemEdit.kt
+++ app/src/main/java/com/shoppinglist/views/ItemEdit.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
--- app/src/main/java/com/shoppinglist/views/Lists.kt
+++ app/src/main/java/com/shoppinglist/views/Lists.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccessTime
@@ -43,6 +44,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
@@ -161,6 +163,9 @@ fun Lists(viewModel: RoomViewModel, navController: NavHostController) {
                             },
                             modifier = Modifier.fillMaxWidth(),
                             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+                            keyboardActions = KeyboardActions {
+                                focusManager.moveFocus(FocusDirection.Next)
+                            },
                             singleLine = true,
                             isError = listNameMissing,
                             supportingText = {
